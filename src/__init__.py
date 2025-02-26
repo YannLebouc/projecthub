ad5ebb11adfdbce6d9653eba8e5f155fbe607dce
Lebouc Yann <yann.leb251@gmail.com>
2025-02-26 07:10:58 +0100

    Added first blueprint to test user route

--- src/__init__.py
+++ src/__init__.py
@@ -2,6 +2,7 @@ import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from src.config import config_dict
+from src.controllers.user_controller import user_bp
 from dotenv import load_dotenv
 
 # Load environment variables from .env file
@@ -20,4 +21,11 @@ def create_app():
     # Initialize database
     db.init_app(app)
 
+    # Initialize blueprints
+    app.register_blueprint(user_bp, url_prefix="/users")
+
     return app
+
+if __name__ == "__main__":
+    app = create_app()
+    app.run(debug=True)
---
+++ src/controllers/user_controller.py
@@ -0,0 +1,8 @@
+from flask import Blueprint, jsonify, request
+
+# Déclaration du Blueprint
+user_bp = Blueprint("user", __name__)
+
+@user_bp.route("/", methods=["GET"])
+def get_users():
+        return "COUCOU"
